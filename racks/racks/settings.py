8a37e954c3c1567da65dfdb72f18571301f575b0
5lunk <desaccorde@gmail.com>
2022-11-24 12:11:57 +0400

    add docker

--- racks/racks/settings.py
+++ racks/racks/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'phh(-1t&3xe*0+asx(81wi(+o^*=42b1ajbc+p38_7ka6u%v&x'
+#SECRET_KEY = 'phh(-1t&3xe*0+asx(81wi(+o^*=42b1ajbc+p38_7ka6u%v&x'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", default=1)))
 
-ALLOWED_HOSTS = [os.environ.get('LOCALHOST')]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 CORS_ALLOWED_ORIGINS = [
     f"http://{os.environ.get('LOCALHOST')}:{os.environ.get('VUE_PORT')}",
--- racks_frontend/src/main.js
+++ racks_frontend/src/main.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 import './assets/tailwind.css'
 
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
 
 createApp(App).use(store).use(router, axios).mount('#app')
 
