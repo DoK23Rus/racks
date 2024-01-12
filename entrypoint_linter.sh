6cfe45794d901890d78ef5ab722e40a2ac77ce17
dzmartli <desaccorde@gmail.com>
2024-01-12 18:54:18 +0400

    delete old/prepare for new

--- entrypoint_linter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-FLAKE8_LOG="${BASE_APP_DIR}${LOGS_PATH}lake8_$(date +'%Y-%m-%d_%I-%M-%S').log"
-touch $FLAKE8_LOG &&
-chmod 666 $FLAKE8_LOG &&
-flake8 ./mainapp > $FLAKE8_LOG 2>&1 &&
-
-exec "$@"
--- entrypoint_typing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-MYPY_LOG="${BASE_APP_DIR}${LOGS_PATH}mypy_$(date +'%Y-%m-%d_%I-%M-%S').log"
-touch $MYPY_LOG &&
-chmod 666 $MYPY_LOG &&
-mypy ./mainapp > $MYPY_LOG 2>&1 &&
-
-exec "$@"
