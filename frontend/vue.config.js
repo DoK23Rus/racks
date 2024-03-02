73a7166ec33bb2ec27f2627237865a132b95b7ca
dzmartli <desaccorde@gmail.com>
2024-03-02 14:39:40 +0400

    test env fix

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    allowedHosts: "all"
+  },
   configureWebpack: {
     watchOptions: {
       aggregateTimeout: 200,
